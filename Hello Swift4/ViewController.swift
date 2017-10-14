e16942fbc693bf7fae0080800dff391e43c6e126
khaiharnn <khaiharnn@gmail.com>
2017-10-14 11:52:17 +0700

    OK

---
+++ Hello Swift4/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Hello Swift4
+//
+//  Created by Patinya Sathaporncharoenying on 10/14/2560 BE.
+//  Copyright © 2560 khaiharnn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Explicit การประกาศตัวแปรแบบกำหนดค่า
+    var nameString: String = ""
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    } // Main Function สิ่งแรกที่จะทำงาน
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+} // Main Class
